c998ea9caca8b2bc95b9c0a3d2ad72ed446643a7
Tomáš Kouba <tomas.kouba@gooddata.com>
2023-02-23 14:19:16 +0100

    Add cfg file for wifi, leds and output pin.
    Also start over when any exception occurs.

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # Warning
 
-The code is not very generic (hardcoded number of leds in my strip)
-nor very robust (dies on exceptions and does not start over).
-
-The SSID and wifi password needs to be provided before uploaded to pico w.
-
 Use 'thonny' to save/upload the code to pico w (when the uf2 with micropython is
 already there).
+The config.json can be uploaded to the pico w via usb mass storage mechanism.
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import machine, network, socket, utime, neopixel
+import json
+
+# Load config and set global variables
+config = json.load(open('config.json'))
+SSID = config['ssid']
+WIFI_PASSWORD = config['pass']
+LED_COUNT = config['led_count']
+DATA_PIN = config['data_pin']
 
 print("Setting network to client mode")
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 
-print("Connecting to rentom_IoT")
+print(f"Connecting to {SSID}")
 sta_if.connect(SSID, WIFI_PASSWORD)
 while not sta_if.isconnected():
     print("We are not connected yet")
@@ -19,31 +27,34 @@ s.listen(1)
 
 print('listening on', addr)
 print('GET /<r>/<g>/<b>/1/2/... => switch leds with addresses 1,2,... to RGB values')
-print('the leds are 0-22')
+print(f'there are {led_count} leds')
 
-np = neopixel.NeoPixel(machine.Pin(28), 23)
+np = neopixel.NeoPixel(machine.Pin(DATA_PIN), LED_COUNT)
 
 while True:
-    cl, addr = s.accept()
-    print('client connected from', addr)
-    cl_file = cl.makefile('rwb', 0)
-    leds = []
-    r = g = b = 0
-    while True:
-        line = cl_file.readline()
-        if not line or line == b'\r\n':
-            break
-        else:
-            if line.split()[0]==b'GET':
-                _,r,g,b,res = line.split()[1].split(b'/', 4)
-                r = int(r)
-                g = int(g)
-                b = int(b)
-                print(r,g,b)
-                print(res)
-                for i in res.split(b'/'):
-                    np[int(i)] = (r,g,b)
-                np.write()
-    cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
-    cl.send('\n')
-    cl.close()
+    try:
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        cl_file = cl.makefile('rwb', 0)
+        leds = []
+        r = g = b = 0
+        while True:
+            line = cl_file.readline()
+            if not line or line == b'\r\n':
+                break
+            else:
+                if line.split()[0]==b'GET':
+                    _,r,g,b,res = line.split()[1].split(b'/', 4)
+                    r = int(r)
+                    g = int(g)
+                    b = int(b)
+                    print(r,g,b)
+                    print(res)
+                    for i in res.split(b'/'):
+                        np[int(i)] = (r,g,b)
+                    np.write()
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send('\n')
+        cl.close()
+    except Exception as exc:
+        print(f"Exception caught: {exc}")
